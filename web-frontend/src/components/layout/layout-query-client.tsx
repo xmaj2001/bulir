3b9d00a8d37a2b8c4b0f868081ed72323c877fe5
xmaj2001 <xmaj2001@gmail.com>
2026-01-06 05:28:50 -0500

    chore: add TanStack Query for server state management

---
+++ web-frontend/src/components/layout/layout-query-client.tsx
@@ -0,0 +1,19 @@
+'use client';
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+export default function QueryLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
